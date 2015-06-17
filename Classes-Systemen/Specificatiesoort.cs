2522742aab66762a010450cf3cca79ec8f1ea01c
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-06-17 09:28:37 +0200

    Update menubalk en achterliggende classes voor zoeksysteem

---
+++ Classes-Systemen/Specificatiesoort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndividueleOpdrachtSE2
+{
+    public class Specificatiesoort
+    {
+        public int ID { get; set; }
+
+        public Productgroep ProductGroep { get; set; }
+
+        public string Naam { get; set; }
+
+        public Specificatiesoort(int id, Productgroep productGroep, string naam)
+        {
+            this.ID = id;
+            this.ProductGroep = productGroep;
+            this.Naam = naam;
+        }
+    }
+}
---
+++ Classes-Systemen/Zoeksysteem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndividueleOpdrachtSE2
+{
+    public class Zoeksysteem
+    {
+    }
+}
